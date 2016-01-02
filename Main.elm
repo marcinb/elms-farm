0d3b1a537bfad312d2d25a8139a4e49b6bac3d9b
Marcin Balinski <balinski@gmail.com>
2016-01-02 22:29:05 +0100

    Render default map

--- Main.elm
+++ Main.elm
@@ -8,24 +8,60 @@ import Keyboard
 import Window
 import Time
 
+import Array
+
 type alias TileSet = 
   { path : String,
     tileSize: (Int, Int)
   }
    
+defaultTileSize : (Int, Int) 
+defaultTileSize =
+  (32, 32)
+
 createTileSet : String -> TileSet
 createTileSet filename =
   let
     tileSetPath = "assets/tiles/" ++ filename
   in
      { path = tileSetPath,
-       tileSize = (32, 32)
+       tileSize = defaultTileSize
      }
 
 plowedSoilTiles : TileSet
 plowedSoilTiles = createTileSet "plowed_soil.png"
 
+mapWidth : Int
+mapWidth = 10
+
+mapHeight : Int
+mapHeight = 10
+
+type alias Map = Array.Array Int
+
+defaultMap : Map
+defaultMap =
+  Array.repeat (mapWidth*mapHeight) 0
+
 -- VIEW
+
+mapToForms : Map -> List Form
+mapToForms gameMap =
+  let
+    groundTile = tile plowedSoilTiles (0,5)
+    (tileW, tileH) = (32.0, 32.0)
+    mappingFn = (\i _ -> 
+      let column = i % mapWidth
+          row = i // mapHeight
+          offsetX = (toFloat column) * tileW
+          offsetY = (toFloat row) * tileH
+      in
+        move (offsetX, offsetY) groundTile
+      )
+  in
+    Array.indexedMap mappingFn gameMap
+      |> Array.toList
+
 
 tile : TileSet -> (Int, Int) -> Form
 tile tileSet (x,y) =
@@ -37,8 +73,7 @@ tile tileSet (x,y) =
 
 view : (Int, Int) -> Element
 view (w,h) =
-    collage w h
-      [tile plowedSoilTiles (0,5)]
+    collage w h (mapToForms defaultMap)
 
 main : Signal Element
 main =
